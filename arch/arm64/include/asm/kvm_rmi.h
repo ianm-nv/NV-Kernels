41774a9e18a8fab54f73096ab4cd89848e9e34b4
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-12-08 09:04:29 +0000

    NVIDIA: SAUCE: arm64: RMI: Propagate max SVE vector length from RMM
    RMM provides the maximum vector length it supports for a guest in its
    feature register. Make it visible to the rest of KVM and to userspace
    via KVM_REG_ARM64_SVE_VLS.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    [cherry-picked from cca-host/v13 56744e428557]
    Signed-off-by: Ian May <ianm@nvidia.com>

--- arch/arm64/include/asm/kvm_rmi.h
+++ arch/arm64/include/asm/kvm_rmi.h
@@ -87,6 +87,7 @@ struct realm_rec {
 void kvm_init_rmi(void);
 u32 kvm_realm_ipa_limit(void);
 u8 kvm_realm_max_pmu_counters(void);
+unsigned int kvm_realm_sve_max_vl(void);
 
 u64 kvm_realm_reset_id_aa64dfr0_el1(const struct kvm_vcpu *vcpu, u64 val);
 
