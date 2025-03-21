b2953369f51743e1adf14c1acc9a706a9c09fdd9
Arto Merilainen <amerilainen@nvidia.com>
2025-03-21 12:10:42 +0200

    NVIDIA: VR: SAUCE: virt: rmeda: RMEDA Host Library
    Introduce a library to register devices with RMM and attach
    attached devices to realms.
    
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Signed-off-by: Ian May <ianm@nvidia.com>

---
+++ drivers/virt/rmeda/rmeda_common.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * SPDX-FileCopyrightText: Copyright (C) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ */
+
+#ifndef __RMEDA_COMMON_H_
+#define __RMEDA_COMMON_H_
+
+#define RHI_DA_CONTROL_IDE		0xCC000055
+
+#endif /* __RMEDA_COMMON_H_ */
---
+++ include/linux/rmeda_host.h
@@ -0,0 +1,58 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * SPDX-FileCopyrightText: Copyright (C) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ */
+
+#ifndef RMEDA_HOST_H
+#define RMEDA_HOST_H
+
+#include <linux/types.h>
+
+struct pci_dev;
+struct rmeda_host;
+struct kvm;
+
+#if IS_ENABLED(CONFIG_RMEDA_HOST)
+
+void rmeda_host_unregister(struct rmeda_host *rmeda_host);
+struct rmeda_host *rmeda_host_register(struct pci_dev *pdev,
+				       bool platform_dev,
+				       struct resource *ncoh_res,
+				       size_t n_ncoh_res,
+				       struct resource *coh_res,
+				       size_t n_coh_res);
+void rmeda_host_detach(struct rmeda_host *rmeda_host);
+int rmeda_host_attach(struct rmeda_host *rmeda_host,
+		      struct kvm *kvm,
+		      u64 vdev_id);
+
+#else
+
+static inline void rmeda_host_unregister(struct rmeda_host *rmeda_host)
+{
+}
+
+struct rmeda_host *rmeda_host_register(struct pci_dev *pdev,
+				       bool platform_dev,
+				       struct resource *ncoh_res,
+				       size_t n_ncoh_res,
+				       struct resource *coh_res,
+				       size_t n_coh_res)
+{
+	return NULL;
+}
+
+static inline void rmeda_host_detach(struct rmeda_host *rmeda_host)
+{
+}
+
+static inline int rmeda_host_attach(struct rmeda_host *rmeda_host,
+				    struct kvm *kvm,
+				    u64 vdev_id)
+{
+	return -ENOTTY;
+}
+
+#endif /* defined(CONFIG_RMEDA_HOST) || defined(CONFIG_RMEDA_HOST_MODULE) */
+
+#endif /* RMEDA_HOST_H */
