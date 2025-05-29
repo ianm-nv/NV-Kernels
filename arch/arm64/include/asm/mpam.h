kernelorg-mirror/linux_kernel_git_morse_linux

arm64: mpam: Add helpers to change a tasks and cpu mpam partid/pmg values
Care must be taken when modifying the partid and pmg of a task, as
writing these values may race with the task being scheduled in, and
reading the modified values.

Add helpers to set the task properties, and the cpu default value,
and add the plumbing to the mpam driver that lets resctrl use them.

Signed-off-by: James Morse <james.morse@arm.com>

arch/arm64/include/asm/mpam.h
@@ -5,6 +5,7 @@
 #define __ASM__MPAM_H
 
 #include <linux/bitops.h>
+#include <linux/bitfield.h>
 #include <linux/init.h>
 #include <linux/jump_label.h>
 #include <linux/percpu.h>
@@ -89,12 +90,55 @@ static inline void __init __enable_mpam_hcr(void)
  * A value in struct thread_info is used instead of struct task_struct as the
  * cpu's u64 register format is used, but struct task_struct has two u32'.
  */
+ static inline void mpam_set_cpu_defaults(int cpu, u16 partid_d, u16 partid_i,
+					  u8 pmg_d, u8 pmg_i)
+{
+	u64 default_val;
+
+	default_val = FIELD_PREP(MPAM_SYSREG_PARTID_D, partid_d);
+	default_val |= FIELD_PREP(MPAM_SYSREG_PARTID_I, partid_i);
+	default_val |= FIELD_PREP(MPAM_SYSREG_PMG_D, pmg_d);
+	default_val |= FIELD_PREP(MPAM_SYSREG_PMG_I, pmg_i);
+
+	WRITE_ONCE(per_cpu(arm64_mpam_default, cpu), default_val);
+}
+
+static inline void mpam_set_task_partid_pmg(struct task_struct *tsk,
+					    u16 partid_d, u16 partid_i,
+					    u8 pmg_d, u8 pmg_i)
+{
+	u64 regval;
+
+	regval = FIELD_PREP(MPAM_SYSREG_PARTID_D, partid_d);
+	regval |= FIELD_PREP(MPAM_SYSREG_PARTID_I, partid_i);
+	regval |= FIELD_PREP(MPAM_SYSREG_PMG_D, pmg_d);
+	regval |= FIELD_PREP(MPAM_SYSREG_PMG_I, pmg_i);
+
+#ifdef CONFIG_ARM64_MPAM
+	WRITE_ONCE(task_thread_info(tsk)->mpam_partid_pmg, regval);
+#endif
+}
+
 static inline u64 mpam_get_regval(struct task_struct *tsk)
 {
 #ifdef CONFIG_ARM64_MPAM
 	return READ_ONCE(task_thread_info(tsk)->mpam_partid_pmg);
 #else
 	return 0;
+#endif
+}
+
+static inline void resctrl_arch_set_rmid(struct task_struct *tsk, u32 rmid)
+{
+	u64 regval = mpam_get_regval(tsk);
+
+	regval &= ~MPAM_SYSREG_PMG_D;
+	regval &= ~MPAM_SYSREG_PMG_I;
+	regval |= FIELD_PREP(MPAM_SYSREG_PMG_D, rmid);
+	regval |= FIELD_PREP(MPAM_SYSREG_PMG_I, rmid);
+
+#ifdef CONFIG_ARM64_MPAM
+	WRITE_ONCE(task_thread_info(tsk)->mpam_partid_pmg, regval);
 #endif
 }
 

ianm-nv/NV-Kernels

(cherry picked from commit 2549e6420f60780c1415f4e9741cd9da5f24b428 https://web.git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git/log/?h=mpam/snapshot/v6.14-rc1)
Signed-off-by: Koba Ko <kobak@nvidia.com>
Reviewed-by: Fenghua Yu <fenghuay@nvidia.com>
Reviewed-by: Tushar Dave <tdave@nvidia.com>
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

arch/arm64/include/asm/mpam.h
@@ -5,6 +5,7 @@
 #define __ASM__MPAM_H
 
 #include <linux/bitops.h>
+#include <linux/bitfield.h>
 #include <linux/init.h>
 #include <linux/jump_label.h>
 #include <linux/percpu.h>
@@ -45,12 +46,55 @@ static inline void __init __enable_mpam_hcr(void)
  * A value in struct thread_info is used instead of struct task_struct as the
  * cpu's u64 register format is used, but struct task_struct has two u32'.
  */
+static inline void mpam_set_cpu_defaults(int cpu, u16 partid_d, u16 partid_i,
+					 u8 pmg_d, u8 pmg_i)
+{
+	u64 default_val;
+
+	default_val = FIELD_PREP(MPAM1_EL1_PARTID_D, partid_d);
+	default_val |= FIELD_PREP(MPAM1_EL1_PARTID_I, partid_i);
+	default_val |= FIELD_PREP(MPAM1_EL1_PMG_D, pmg_d);
+	default_val |= FIELD_PREP(MPAM1_EL1_PMG_I, pmg_i);
+
+	WRITE_ONCE(per_cpu(arm64_mpam_default, cpu), default_val);
+}
+
+static inline void mpam_set_task_partid_pmg(struct task_struct *tsk,
+					    u16 partid_d, u16 partid_i,
+					    u8 pmg_d, u8 pmg_i)
+{
+#ifdef CONFIG_ARM64_MPAM
+	u64 regval;
+
+	regval = FIELD_PREP(MPAM1_EL1_PARTID_D, partid_d);
+	regval |= FIELD_PREP(MPAM1_EL1_PARTID_I, partid_i);
+	regval |= FIELD_PREP(MPAM1_EL1_PMG_D, pmg_d);
+	regval |= FIELD_PREP(MPAM1_EL1_PMG_I, pmg_i);
+
+	WRITE_ONCE(task_thread_info(tsk)->mpam_partid_pmg, regval);
+#endif
+}
+
 static inline u64 mpam_get_regval(struct task_struct *tsk)
 {
 #ifdef CONFIG_ARM64_MPAM
 	return READ_ONCE(task_thread_info(tsk)->mpam_partid_pmg);
 #else
 	return 0;
+#endif
+}
+
+static inline void resctrl_arch_set_rmid(struct task_struct *tsk, u32 rmid)
+{
+#ifdef CONFIG_ARM64_MPAM
+	u64 regval = mpam_get_regval(tsk);
+
+	regval &= ~MPAM1_EL1_PMG_D;
+	regval &= ~MPAM1_EL1_PMG_I;
+	regval |= FIELD_PREP(MPAM1_EL1_PMG_D, rmid);
+	regval |= FIELD_PREP(MPAM1_EL1_PMG_I, rmid);
+
+	WRITE_ONCE(task_thread_info(tsk)->mpam_partid_pmg, regval);
 #endif
 }
 