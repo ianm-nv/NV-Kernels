kernelorg-mirror/linux_kernel_git_morse_linux

arm_mpam: Allow MBWU counters to be used, even when not free running
Resctrl exposes teh cache opccupancy and bandwidth counters to user space
via files. Each control and monitor group has a copy of these files, and
can read the counter by reading the file.

MPAM needs to allocate a hardware monitor to do the counting work, and there
may not be enough for every control and monitor group to have one. Currently
MPAM doesn't expose the MBM/MBWU counters to resctrl unless there are enough
monitors.

To allow perf to read these counters via a PMU driver, add support for
allocating a monitor. To prevent these appearing in the resctrl filesystem,
report false from resctrl_arch_event_is_free_running() for the MBM event
types.

Signed-off-by: James Morse <james.morse@arm.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -51,6 +51,8 @@ static bool resctrl_enabled;
 static bool cacheinfo_ready;
 static DECLARE_WAIT_QUEUE_HEAD(wait_cacheinfo_ready);
 
+bool mpam_monitors_free_runing;
+
 bool resctrl_arch_alloc_capable(void)
 {
 	return exposed_alloc_capable;
@@ -290,7 +292,11 @@ int resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r, int evtid)
 
 		return mpam_alloc_csu_mon(res->class);
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
-		return USE_RMID_IDX;
+		if (mpam_monitors_free_runing)
+			return USE_RMID_IDX;
+		res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+
+		return mpam_alloc_mbwu_mon(res->class);
 	case QOS_L3_MBM_TOTAL_EVENT_ID:
 	default:
 		return -EOPNOTSUPP;
@@ -308,8 +314,16 @@ void resctrl_arch_mon_ctx_free(struct rdt_resource *r, int evtid, int ctx)
 		mpam_free_csu_mon(res->class, ctx);
 		wake_up(&resctrl_mon_ctx_waiters);
 		return;
-	case QOS_L3_MBM_TOTAL_EVENT_ID:
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
+		if (mpam_monitors_free_runing)
+			return;
+		res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+
+		mpam_free_mbwu_mon(res->class, ctx);
+		wake_up(&resctrl_mon_ctx_waiters);
+		return;
+	case QOS_L3_MBM_TOTAL_EVENT_ID:
+	default:
 		return;
 	}
 }
@@ -451,15 +465,21 @@ static bool class_has_usable_mbwu(struct mpam_class *class)
 	if (!mpam_has_feature(mpam_feat_msmon_mbwu, cprops))
 		return false;
 
-	/*
-	 * resctrl expects the bandwidth counters to be free running,
-	 * which means we need as many monitors as resctrl has
-	 * control/monitor groups.
-	 */
-	if (cprops->num_mbwu_mon < resctrl_arch_system_num_rmid_idx())
+	if ((mpam_partid_max <= 1) && (mpam_pmg_max == 0))
 		return false;
 
-	return (mpam_partid_max > 1) || (mpam_pmg_max != 0);
+	/*
+	 * resctrl expects the bandwidth counters to be free running,
+	 * which means to expose the files in the filesystem we need
+	 * as many monitors as resctrl has control/monitor groups.
+	 * Otherwise, these counters are only accessible via perf.
+	 */
+	if (cprops->num_mbwu_mon < resctrl_arch_system_num_rmid_idx())
+		mpam_monitors_free_runing = false;
+	else
+		mpam_monitors_free_runing = true;
+
+	return true;
 }
 
 static bool mba_class_use_mbw_part(struct mpam_props *cprops)

ianm-nv/NV-Kernels

Resctrl exposes the cache opccupancy and bandwidth counters to user space
Signed-off-by: Tushar Dave <tdave@nvidia.com>
(cherry picked from commit 0241ab3984ff0f2ecf205e1eb6e4eb0ff19f2a5e https://github.com/NVIDIA-BaseOS-6/linux-nvidia-6.5/tree/MPAM-dev)
Signed-off-by: Koba Ko <kobak@nvidia.com>
Reviewed-by: Fenghua Yu <fenghuay@nvidia.com>
Reviewed-by: Tushar Dave <tdave@nvidia.com>
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

drivers/platform/arm64/mpam/mpam_resctrl.c
@@ -63,6 +63,9 @@ static bool resctrl_enabled;
 static bool cacheinfo_ready;
 static DECLARE_WAIT_QUEUE_HEAD(wait_cacheinfo_ready);
 
+/* Whether counters were pre-allocated and are left running */
+bool mpam_monitors_free_runing;
+
 /* A dummy mon context to use when the monitors were allocated up front */
 u32 __mon_is_rmid_idx = USE_RMID_IDX;
 void *mon_is_rmid_idx = &__mon_is_rmid_idx;
@@ -351,7 +354,12 @@ static void *resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r,
 		return ret;
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
 	case QOS_L3_MBM_TOTAL_EVENT_ID:
-		return mon_is_rmid_idx;
+		if (mpam_monitors_free_runing)
+			return mon_is_rmid_idx;
+		res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+
+		*ret = mpam_alloc_mbwu_mon(res->class);
+		return ret;
 	}
 
 	return ERR_PTR(-EOPNOTSUPP);
@@ -395,8 +403,16 @@ void resctrl_arch_mon_ctx_free(struct rdt_resource *r,
 		mpam_free_csu_mon(res->class, mon);
 		wake_up(&resctrl_mon_ctx_waiters);
 		return;
-	case QOS_L3_MBM_TOTAL_EVENT_ID:
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
+		if (mpam_monitors_free_runing)
+			return;
+		res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+
+		mpam_free_mbwu_mon(res->class, mon);
+		wake_up(&resctrl_mon_ctx_waiters);
+		return;
+	case QOS_L3_MBM_TOTAL_EVENT_ID:
+	default:
 		return;
 	}
 }
@@ -604,13 +620,16 @@ static bool class_has_usable_mbwu(struct mpam_class *class)
 
 	/*
 	 * resctrl expects the bandwidth counters to be free running,
-	 * which means we need as many monitors as resctrl has
-	 * control/monitor groups.
+	 * which means to expose the files in the filesystem we need
+	 * as many monitors as resctrl has control/monitor groups.
+	 * Otherwise, these counters are only accessible via perf.
 	 */
 	if (cprops->num_mbwu_mon < resctrl_arch_system_num_rmid_idx())
-		return false;
+		mpam_monitors_free_runing = false;
+	else
+		mpam_monitors_free_runing = true;
 
-	return (mpam_partid_max > 1) || (mpam_pmg_max != 0);
+	return true;
 }
 
 /*