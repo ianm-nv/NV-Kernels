nicolinc/iommufd

iommufd/selftest: Add coverage for the new mmap interface
Extend the loopback test to a new mmap page.

Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

tools/testing/selftests/iommu/iommufd.c
@@ -2777,12 +2777,17 @@ TEST_F(iommufd_viommu, viommu_alloc_with_data)
 	struct iommu_viommu_selftest data = {
 		.in_data = 0xbeef,
 	};
+	uint32_t *test;
 
 	if (self->device_id) {
 		test_cmd_viommu_alloc(self->device_id, self->hwpt_id,
 				      IOMMU_VIOMMU_TYPE_SELFTEST, &data,
 				      sizeof(data), &self->viommu_id);
 		assert(data.out_data == data.in_data);
+		test = mmap(NULL, data.out_mmap_size, PROT_READ | PROT_WRITE,
+			    MAP_SHARED, self->fd, data.out_mmap_base);
+		assert(test && *test == data.in_data);
+		munmap(test, data.out_mmap_size);
 	}
 }
 

ianm-nv/NV-Kernels

(cherry picked from commit 6b1d05bbe7cf2428ff6cf4a38a27bcec66396947 https://github.com/nicolinc/iommufd/commits/wip/iommufd_vcmdq-v1)
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
Acked-by: Carol L. Soto <csoto@nvidia.com>
Acked-by: Koba Ko <kobak@nvidia.com>

tools/testing/selftests/iommu/iommufd.c
@@ -2777,12 +2777,17 @@ TEST_F(iommufd_viommu, viommu_alloc_with_data)
 	struct iommu_viommu_selftest data = {
 		.in_data = 0xbeef,
 	};
+	uint32_t *test;
 
 	if (self->device_id) {
 		test_cmd_viommu_alloc(self->device_id, self->hwpt_id,
 				      IOMMU_VIOMMU_TYPE_SELFTEST, &data,
 				      sizeof(data), &self->viommu_id);
 		assert(data.out_data == data.in_data);
+		test = mmap(NULL, data.out_mmap_pgsz, PROT_READ | PROT_WRITE,
+			    MAP_SHARED, self->fd, data.out_mmap_pgoff);
+		assert(test && *test == data.in_data);
+		munmap(test, data.out_mmap_pgsz);
 	}
 }
 