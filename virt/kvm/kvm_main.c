1ac9567cc3348b626d467eb7fd68738a5104526c
Ian May <ianm@nvidia.com>
2026-04-14 20:40:15 +0000

    NVIDIA: VR: SAUCE: kvm: Export gfn_to_hva_memslot_prot for RMEDA
    Add annotations: Enable RMEDA modules for arm64 nvidia flavour
    
    Signed-off-by: Ian May <ianm@nvidia.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -2766,6 +2766,8 @@ unsigned long gfn_to_hva_memslot_prot(struct kvm_memory_slot *slot,
 	return hva;
 }
 
+EXPORT_SYMBOL_GPL(gfn_to_hva_memslot_prot);
+
 unsigned long gfn_to_hva_prot(struct kvm *kvm, gfn_t gfn, bool *writable)
 {
 	struct kvm_memory_slot *slot = gfn_to_memslot(kvm, gfn);
